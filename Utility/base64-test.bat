908421911222dc1bfe0355a99e180e497b1725a6
Michel OMEN-40L <michelmarrazzo@gmail.com>
2022-11-02 19:46:36 +0100

    Update Utility/base64-test.bat

--- Utility/base64-test.bat
+++ Utility/base64-test.bat
@@ -1,5 +1,12 @@
 @echo off
+:: COMMENT
+:: © 2012 https://github.com/Michel-IT. All rights reserved.
+:: You can use this file and disclose it. 
+:: Please do not remove this comment. 
+:: Out of respect for intellectual property.
+
 cd ../Utility/
+
 for /f "tokens=* delims=" %%# in ('base64.bat -decode RnJhZ29sYTg4') do set "decoded=%%#"
 echo %decoded% 
 
